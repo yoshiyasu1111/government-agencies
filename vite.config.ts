659d37625db702aedd1cdd46f1237b3dfe225e42
yoshiyasu <56212616+yoshiyasu1111@users.noreply.github.com>
2026-08-11 20:12:08 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  site: 'https://yoshiyasu1111.github.io',
+  base: '/government-agencies',
   plugins: [
     tailwindcss(),
   ],
